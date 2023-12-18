9f74ecbfaab962499debed3decfaadcf3fdc6183
peter115342 <pmuzslay8@gmail.com>
2023-12-18 13:11:48 +0100

    Added rendering  and movement with arrow keys

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,25 @@
 <script setup>
+import GameRender from './components/GameRender.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-
+    <div class="main-container">
+      <GameRender />
     </div>
-  </header>
-
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+body {
+  margin: 0;
   text-align: center;
-  margin-top: 2rem;
+}
+.main-container{
+margin: 0 auto;
+position: relative;
+width: 600px;
+height: 596px;
+margin-top: 50px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/components/GameRender.vue
+++ src/components/GameRender.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-    
+    <div class="render" ref="renderWindow" @keydown="handleKeyDown" tabindex="0">
+      <img :style="{ left: `${imagePosition.x}px`, top: `${imagePosition.y}px`, width: `55px` }" src="/images/car_sport.png" alt="Car" />
     </div>
   </template>
   
@@ -8,20 +8,50 @@
   export default {
     data() {
       return {
-       
+        imagePosition: { x: 250, y: 350 },
+        moveStep: 10,
+        imageWidth: 55,
       };
     },
     methods: {
-      // You can define methods here
+      handleKeyDown(event) {
+        this.$nextTick(() => {
+          const renderWindowWidth = this.$refs.renderWindow.clientWidth;
+  
+          switch (event.key) {
+            case 'ArrowLeft':
+              this.moveImage(-this.moveStep, 0, renderWindowWidth);
+              break;
+            case 'ArrowRight':
+              this.moveImage(this.moveStep, 0, renderWindowWidth);
+              break;
+          }
+        });
+      },
+      moveImage(deltaX, deltaY, renderWindowWidth) {
+        // Calculate the new x-coordinate
+        const newX = this.imagePosition.x + deltaX;
+  
+        if (newX >= 0 && newX + this.imageWidth <= renderWindowWidth) {
+          this.imagePosition.x = newX;
+        }
+      },
     },
-    // You can add more lifecycle hooks, computed properties, etc.
   };
   </script>
   
   <style scoped>
-  /* Add your component-specific styles here */
-  h1 {
-    color: #42b983;
+  .render {
+  width: 100%;
+  height: 100%;
+  background-image: url("/images/background_road.png");
+  background-size: cover;
+  position: relative;
+  margin: 0 auto;
+}
+  
+  img {
+    position: absolute;
   }
   </style>
   
