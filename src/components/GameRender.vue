f6a473a3fdecb4ee06dc7fbc080155e7645de710
peter115342 <pmuzslay8@gmail.com>
2023-12-18 10:02:36 +0100

    installed PWA plugin and made PNGs for cars

---
+++ src/components/GameRender.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+       
+      };
+    },
+    methods: {
+      // You can define methods here
+    },
+    // You can add more lifecycle hooks, computed properties, etc.
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your component-specific styles here */
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,6 +8,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate'
+    }),
   ],
   resolve: {
     alias: {
