00295105f990a89d603aa4f979f9731ae32702b4
peter115342 <pmuzslay8@gmail.com>
2023-12-18 14:17:07 +0100

    car centering when mounted

--- src/components/GameRender.vue
+++ src/components/GameRender.vue
@@ -8,25 +8,23 @@
   export default {
     data() {
       return {
-        imagePosition: { x: 250, y: 350 },
-        moveStep: 10,
+        imagePosition: { x: 0, y: 0 },
+        moveStep: 3,
         imageWidth: 55,
       };
     },
     methods: {
       handleKeyDown(event) {
-        this.$nextTick(() => {
-          const renderWindowWidth = this.$refs.renderWindow.clientWidth;
+        const renderWindowWidth = this.$refs.renderWindow.clientWidth;
   
-          switch (event.key) {
-            case 'ArrowLeft':
-              this.moveImage(-this.moveStep, 0, renderWindowWidth);
-              break;
-            case 'ArrowRight':
-              this.moveImage(this.moveStep, 0, renderWindowWidth);
-              break;
-          }
-        });
+        switch (event.key) {
+          case 'ArrowLeft':
+            this.moveImage(-this.moveStep, 0, renderWindowWidth);
+            break;
+          case 'ArrowRight':
+            this.moveImage(this.moveStep, 0, renderWindowWidth);
+            break;
+        }
       },
       moveImage(deltaX, deltaY, renderWindowWidth) {
         // Calculate the new x-coordinate
@@ -37,18 +35,39 @@
         }
       },
     },
+    mounted() {
+      this.imagePosition.x = (this.$refs.renderWindow.clientWidth - this.imageWidth) / 2;
+      this.imagePosition.y = (this.$refs.renderWindow.clientHeight - this.imageWidth) / 1.25;
+  
+      // Add event listener for keydown events on the document
+      document.addEventListener('keydown', this.handleKeyDown);
+    },
+    beforeDestroy() {
+      // Remove event listener when the component is destroyed
+      document.removeEventListener('keydown', this.handleKeyDown);
+    },
   };
   </script>
   
   <style scoped>
   .render {
-  width: 100%;
-  height: 100%;
-  background-image: url("/images/background_road.png");
-  background-size: cover;
-  position: relative;
-  margin: 0 auto;
-}
+    width: 100%;
+    height: 100%;
+    background-image: url("/images/background_road.png");
+    background-size: cover;
+    position: relative;
+    margin: 0 auto;
+    animation: scrollBackground 5s linear infinite; /* Adjust the speed (5s) and direction here */
+  }
+  
+  @keyframes scrollBackground {
+    from {
+      background-position: 0 0;
+    }
+    to {
+      background-position: 100% 0;
+    }
+  }
   
   img {
     position: absolute;
