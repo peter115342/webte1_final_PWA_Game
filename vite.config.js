150070d872a41464f451dcb90957867ebffdd7e2
peter115342 <pmuzslay8@gmail.com>
2023-12-25 19:34:24 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,20 @@
-import { fileURLToPath, URL } from 'node:url';
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// Get the current directory
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-public-path/', // Set your public path here
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'autoUpdate'
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    port: 8080, // Set the port to match your Apache configuration
-  },
-  build: {
-    outDir: 'dist', // Set your desired output directory
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
