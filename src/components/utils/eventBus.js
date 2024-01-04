dd1c30dc4a23ea56c778a7197b5690a4ffe96f21
peter115342 <pmuzslay8@gmail.com>
2024-01-04 19:03:48 +0100

    slight fixes

--- src/components/utils/eventBus.js
+++ src/components/utils/eventBus.js
@@ -1,32 +1,28 @@
-// eventBus.js
 import { ref } from 'vue';
 import mitt from 'mitt';
 
-// Create a ref to hold the selected vehicle
 export const selectedVehicle = ref(null);
 const emitter = mitt();
 
-// Create an event bus
 const eventBus = {
   emit: (event, data) => {
-    selectedVehicle.value = data; // Update the selected vehicle
-    emitter.emit(event, data); // Emit the event using mitt
+    selectedVehicle.value = data;
+    emitter.emit(event, data);
     document.dispatchEvent(new CustomEvent(event, { detail: data }));
   },
   on: (event, callback) => {
-    emitter.on(event, callback); // Listen for the event using mitt
+    emitter.on(event, callback);
     document.addEventListener(event, (e) => {
       callback(e.detail);
     });
   },
   off: (event, callback) => {
-    emitter.off(event, callback); // Remove the event listener using mitt
+    emitter.off(event, callback);
     document.removeEventListener(event, callback);
   },
 };
 const events = mitt();
 
-// Define events
 export const EVENTS = {
   START_GAME: 'start-game',
   SET_CURRENT_LEVEL: 'set-current-level',
